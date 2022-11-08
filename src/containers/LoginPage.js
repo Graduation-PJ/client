7adefd526a54c65feba8361921587e72bb68f381
alswnsp411 <alswnsp411@naver.com>
2022-11-09 00:28:43 +0900

    feat: register_page_UI (#4)

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -4,28 +4,29 @@ import NavBar from "./NavBar";
 import {Link} from "react-router-dom";
 
 function LoginPage() {
-    const [id, setId]= useState("");
-    const [password, setPassword]= useState("");
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleLogin=(e)=>{
+    const handleLogin = (e) => {
         e.preventDefault();
     }
 
     return (
         <div className="login_page">
-            <NavBar />
+            <NavBar/>
 
             <div className="login_container">
                 <Link to="/">
                     <span className="logo" style={{fontSize: "50px"}}>로고</span>
                 </Link>
 
-                    <form>
-                        <input type="text" placeholder="아이디"
-                               value={id} onChange={(e)=>setId(e.target.value)} />
-                        <input type="password" placeholder="비밀번호" value={password} onChange={(e)=>setPassword(e.target.value)} />
-                        <button className="login_sign_in_button" type="submit" onClick={handleLogin}>로그인하기</button>
-                    </form>
+                <form>
+                    <input type="text" placeholder="아이디"
+                           value={id} onChange={(e) => setId(e.target.value)}/>
+                    <input type="password" placeholder="비밀번호" value={password}
+                           onChange={(e) => setPassword(e.target.value)}/>
+                    <button className="login_sign_in_button" type="submit" onClick={handleLogin}>로그인하기</button>
+                </form>
 
                 <div className="link_to_register">
                     <span>아직 계정이 없으신가요?</span>
--- src/containers/RegisterPage.js
+++ src/containers/RegisterPage.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './RegisterPage.css';
+import NavBar from "./NavBar";
+import {Link} from "react-router-dom";
 
 function RegisterPage() {
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className="register_page">
-            register_page
+            <NavBar/>
+
+            <div className="register_container">
+                <Link to="/">
+                    <span className="logo" style={{fontSize: "50px"}}>로고</span>
+                </Link>
+
+                <form>
+                    <p>아이디</p>
+                    <input type="text" placeholder="아이디"
+                           value={id} onChange={(e) => setId(e.target.value)}/>
+                    <p>비밀번호</p>
+                    <input type="password" placeholder="비밀번호" value={password}
+                           onChange={(e) => setPassword(e.target.value)}/>
+                    <p>비밀번호 재확인</p>
+                    <input type="password" placeholder="비밀번호 재확인" />
+                    <p>이름</p>
+                    <input type="text" placeholder="이름"
+                           value={name} onChange={(e) => setName(e.target.value)}/>
+                    <p>이메일</p>
+                    <input type="text" placeholder="이메일"
+                           value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <button className="login_sign_in_button" type="submit" onClick={handleRegister}>회원가입하기</button>
+                </form>
+            </div>
         </div>
     );
 }
